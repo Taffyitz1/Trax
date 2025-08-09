2c50cd154a4a6782729ff877071d8cbe35cbfc42
Taffyitz1 <opemipoowolabi7@gmail.com>
2025-08-09 05:21:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -63,11 +63,10 @@ const message = `🚨 NEW CALL 🚨
 🔹 CA: \`${ca}\`  // Only CA in monospace
 🔹 Smart wallet Invested: ${(solAmount / 1e9).toFixed(2)} SOL`;
 
-await sendTelegram(message, "MarkdownV2");  // Use MarkdownV2 for better reliability
+await sendTelegram(message, "MarkdownV2");
 
   res.status(200).send('ok');
-});
-
+ });
 // Telegram alert function
 
 async function sendTelegram(text) {
