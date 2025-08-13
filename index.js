1f2a2cf7ac3afd02b6216c977870024b18997e08
Taffyitz1 <opemipoowolabi7@gmail.com>
2025-08-13 12:21:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.post('/webhook', async (req, res) => {
                        (account !== "Unknown" ? `${account.slice(0, 4)}...${account.slice(-4)}` : "Unknown Wallet");
 
     const stableAndBaseMints = [
-      "So11111111111111111111111111111111111111111", // SOL
+      "So11111111111111111111111111111111111111112", // SOL
       "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v", // USDC
       "Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB"  // USDT
     ].map(m => m.toLowerCase());
